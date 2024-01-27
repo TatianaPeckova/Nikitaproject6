a47b9e88d6f3fc8ef7b1f45ba7faad2480a41219
Name <email@example.ru>
2024-01-27 16:55:22 +0300

    update

--- file.js
+++ file.js
@@ -1,7 +1,20 @@
 // Инициализируем Swiper
-new Swiper(".main__swiper", {
-  pagination: {
-    el: ".main__swiper-pagination",
-    clickable: true,
-  },
+/*new Swiper(".main__swiper", {
+pagination: {
+el: ".main__swiper-pagination",
+clickable: true,
+},
+});*/
+
+const showAll = document.querySelector(".main__show-all");
+const normalHeight = document.querySelector(".wrapper");
+
+showAll.addEventListener("click", (event) => {
+  showAll.classList.toggle("main__show-all_expand");
+
+  showAll.innerHTML = showAll.classList.contains("main__show-all_expand")
+    ? "Скрыть"
+    : "Показать все";
+
+  normalHeight.classList.toggle("wraper-expand");
 });
